fafb976d7ce31cd5e43f7a6af95ba58e02948195
Jeevan Kumar <rg1kumar1@gmail.com>
2022-01-15 18:32:11 +0530

    Face Classifcation implemented!

--- face_classifier.py
+++ face_classifier.py
@@ -1,1 +1,60 @@
 # Classifies detected faces to trained student's faces.
+
+import cv2
+import face_recognition
+from face_recognition.api import face_distance
+from face_scrapper import extract_faces
+import sys
+import os
+
+def classify_faces(students_face_encodings, detected_faces_encodings):
+    students_present_indexes = []
+    for detected_face_encoding in detected_faces_encodings:
+        least_face_distance = 1
+        print ("Face change!")
+        print (students_present_indexes)
+        classified_face_index = None
+        for index, student_face_encoding in enumerate(students_face_encodings):
+            if (face_recognition.compare_faces(student_face_encoding, detected_face_encoding))[0] == True:
+                face_distance = face_recognition.face_distance(student_face_encoding, detected_face_encoding)
+                if  face_distance < least_face_distance:
+                    least_face_distance = face_distance
+                    classified_face_index = index
+        students_present_indexes.append(classified_face_index)
+                
+
+    return students_present_indexes
+            
+
+def load_student_corpus_and_encode(path="face_corpus"):
+    students_path = os.listdir(path)
+    students_face_encodings = []
+    for student in students_path:
+        student_image = face_recognition.load_image_file(f"{path}{os.sep}{student}")
+        student_image = cv2.cvtColor(student_image, cv2.COLOR_BGR2RGB)
+        student_encoding = face_recognition.face_encodings(student_image)
+        students_face_encodings.append(student_encoding)
+        print ("Loaded face ", student)
+
+    return students_face_encodings
+
+
+def map_students_to_indexes(student_indexes, path="face_corpus",):
+    students_file_list = os.listdir(path)
+    mapped_students = []
+    for i in range(len(students_file_list)):
+        students_file_list[i] = students_file_list[i][:-4]
+    
+    for index in student_indexes:
+        mapped_students.append(students_file_list[index])
+
+    return mapped_students
+
+if __name__ == "__main__":
+    detected_faces_encodings, faces_location = extract_faces(sys.argv[1])
+    print ("Faces scraped!")
+    students_face_encodings = load_student_corpus_and_encode()
+    print ("Students faces loaded!")
+    present_student_indexes = classify_faces(students_face_encodings, detected_faces_encodings)
+    present_students = map_students_to_indexes(present_student_indexes)
+    print (present_students)
--- face_scrapper.py
+++ face_scrapper.py
@@ -4,54 +4,27 @@ import cv2
 import sys
 import os
 import face_recognition
+from face_recognition.api import face_encodings
 
 def extract_faces(image_path):
-    
-    # # read image from path
-    # image = cv2.imread(image_path)
-    # grayscale_img = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # # output_img = cv2.imwrite("grayscale_img.jpg", grayscale_img)
-    # # print ("Output Image saved!\n")
-
-    # # Find detected_faces using pre trained cascade model.
-    # faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
-    # detected_faces = faceCascade.detectMultiScale(
-    #     grayscale_img,
-    #     scaleFactor=1.3,
-    #     minNeighbors=3,
-    #     minSize=(30, 30)
-    # )
-
-    # print ("Found {0} detected_faces".format(len(detected_faces)))
-
-    # # Draw a rectangle around the face.
-    # for (x, y, w, h) in detected_faces:
-    #     cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    # # Save the output image to corpus
-    # ouput_path = f'output_corpus{os.sep}output_{image_path[13:]}'
-    # output_img = cv2.imwrite(ouput_path, image)
-    # print ("Detected Faces image saved!\n")
-    # return detected_faces
-
-    input_image =face_recognition.load_image_file(image_path)
+    input_image = face_recognition.load_image_file(image_path)
 
     # Convert input image to RGB
     input_image = cv2.cvtColor(input_image,cv2.COLOR_BGR2RGB)
     
     # Extract Faces from image
-    detected_faces = face_recognition.face_locations(input_image)
+    faces_location = face_recognition.face_locations(input_image)
     output_image = input_image.copy()
     
    # Draw rectangle around detected faces.
-    for face in detected_faces:
+    for face in faces_location:
         cv2.rectangle(output_image, (face[3], face[0]),(face[1], face[2]), (255,0,255), 2)
     cv2.imshow('Detected Faces', output_image)
     cv2.waitKey(0)
 
-    return detected_faces
-
+    faces_encodings = face_recognition.face_encodings(input_image)
+    return (faces_encodings, faces_location)
 
 
 if __name__ == "__main__":
